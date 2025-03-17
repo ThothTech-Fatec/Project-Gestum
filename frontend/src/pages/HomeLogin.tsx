9bbcaccf670c2dbdecb3e1bd60a5b553d54ff68b
HenryBRG <gustavo.henrybraga@gmail.com>
2025-03-17 17:44:34 -0300

    Atualização Avatar e HomeLogin
    Agora é possível acessar o modal de alterações do perfil, além de ser possível realizar o logout. Os projetos agoram são visíveis através de uma lista, também é possivel excluir os mesmos e acessar as informações

--- frontend/src/pages/HomeLogin.tsx
+++ frontend/src/pages/HomeLogin.tsx
@@ -9,13 +9,35 @@ const Home = () => {
     { id: 2, nome: "Projeto B", descricao: "Descrição do Projeto B", responsavel: "João"}
   ]);
 
+  type Projeto = {
+    id:number
+    nome:string
+    descricao:string
+    responsavel:string
+    progresso?: string
+  }
+
   const [modalAberto, setModalAberto] = useState(false);
+  const [modalAbertoProj, setModalAbertoProj] = useState(false);
   const [nomeProjeto, setNomeProjeto] = useState("");
   const [descricaoProjeto, setDescricaoProjeto] = useState("");
   const [responsavel, setResponsavel] = useState("");
   const [mostrarOpcoes, setMostrarOpcoes] = useState(false);
   const [progresso, setProgresso] = useState("");
+  const [projetoSelecionado , setProjetoSelecionado] = useState<Projeto | null>(null);
 
+
+
+  const abrirModalProj = () => {
+    setModalAbertoProj(true);
+  }
+
+  const excluirProjeto = (id: number) => {
+    const projetosAtualizados = projetos.filter((projeto) => projeto.id !== id);
+    setProjetos(projetosAtualizados);
+    fecharModal(); 
+  };
+  
 
   const criarNovoProjeto = () => {
     setModalAberto(true); 
@@ -23,10 +45,12 @@ const Home = () => {
 
   const fecharModal = () => {
     setModalAberto(false);
+    setModalAbertoProj(false);
     setNomeProjeto("");
     setDescricaoProjeto("");
     setResponsavel("");
     setProgresso("");
+    setProjetoSelecionado(null);
 
   };
 
@@ -34,7 +58,7 @@ const Home = () => {
 
 
   const salvarProjeto = () => {
-    if (nomeProjeto.trim() === "" || descricaoProjeto.trim() === "" || responsavel.trim() === " ") {
+    if (nomeProjeto.trim() === "" || descricaoProjeto.trim() === "" || responsavel.trim() === "") {
       alert("Preencha todos os campos!");
       return;
     }
@@ -51,27 +75,70 @@ const Home = () => {
     fecharModal();
   };
 
+  const abrirModalDetalhes = (projeto) => {
+    setProjetoSelecionado(projeto);
+    setModalAbertoProj(true);
+  };
+
 
   return (
-    <div className="container" style={{marginTop: "60px"}}>
-      <SuperiorMenu />
+    <div className="container">
       <main>
+        <SuperiorMenu />
+        <div className="container-button"> 
         <button className="botao-criar" onClick={criarNovoProjeto}>
           Criar Novo Projeto
-        </button>
-        <div className="lista-projetos">
+          </button>
+        </div>
+    
+  
+    <div className="lista-projetos">
           {projetos.map((projeto) => (
             <div key={projeto.id} className="projeto">
+              <div className="projeto-header">
+              <button className="botao-modal" onClick={(e) => { e.stopPropagation();
+               abrirModalDetalhes(projeto) 
+              }}>
+                ...
+              </button>
               <h2>{projeto.nome}</h2>
               <p>{projeto.descricao}</p>
               <p>{projeto.responsavel}</p>
               <ProgressBar/>
             </div>
+            </div>
           ))}
         </div>
       </main>
 
-            {modalAberto && (
+      {modalAbertoProj && projetoSelecionado && (
+  <div className="modal-overlay">
+    <div className="modal-proj">
+    <button className="botao-fechar-proj" onClick={fecharModal}>
+          x
+        </button>
+      <h2>Detalhes do  {projetoSelecionado.nome}</h2>  
+      <p><strong>Descrição:</strong> {projetoSelecionado.descricao}</p>
+      <p><strong>Responsável:</strong> {projetoSelecionado.responsavel}</p>
+      <p style={{ marginLeft: '7%', display: 'flex', 
+  justifyContent: 'center', 
+  alignItems: 'center', 
+  width: '100%',}}><ProgressBar/></p>
+      <div className="botoes">
+        <button className="excluir-proj-home" onClick={() => excluirProjeto(projetoSelecionado.id)}>
+           Excluir
+          </button>
+          <button className="atualizar-proj-home">
+           Atualizar
+          </button>
+
+      </div>
+    </div>
+  </div>
+)}
+
+
+      {modalAberto && (
         <div className="modal-overlay">
           <div className="modal">
             <h2>Criar Novo Projeto</h2>
